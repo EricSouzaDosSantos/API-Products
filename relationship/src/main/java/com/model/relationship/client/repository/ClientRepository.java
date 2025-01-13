e113f1ae70447db9c8e759a50cf4ef3a2dad267b
Eric <ericsouza94895@gmail.com>
2025-01-12 23:51:12 -0300

    feat: add Client Repository

---
+++ relationship/src/main/java/com/model/relationship/client/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.model.relationship.client.repository;
+
+import com.model.relationship.client.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
