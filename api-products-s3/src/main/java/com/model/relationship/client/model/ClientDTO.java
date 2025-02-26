fa004d95c34cb61da99d19ca44d5a97168fdadae
Eric <ericsouza94895@gmail.com>
2025-02-25 23:51:18 -0300

    refactor: change List of DTO

--- api-products-s3/src/main/java/com/model/relationship/client/model/ClientDTO.java
+++ api-products-s3/src/main/java/com/model/relationship/client/model/ClientDTO.java
@@ -2,8 +2,9 @@ package com.model.relationship.client.model;
 
 import com.model.relationship.buy.model.BuyProduct;
 import com.model.relationship.buy.model.BuyProductDTO;
+import com.model.relationship.product.model.Product;
 
 import java.util.List;
 
-public record ClientDTO(String name, String email, String address, List<BuyProductDTO> buyProducts) {
+public record ClientDTO(String name, String email, String address, List<Product> products) {
 }
