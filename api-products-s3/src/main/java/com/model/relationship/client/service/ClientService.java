0631e1e835da1aa1bb3ee775a4c110cadb01f81f
Eric <ericsouza94895@gmail.com>
2025-02-25 23:50:36 -0300

    feat: add method for update and buy product in Service

--- api-products-s3/src/main/java/com/model/relationship/client/service/ClientService.java
+++ api-products-s3/src/main/java/com/model/relationship/client/service/ClientService.java
@@ -8,9 +8,13 @@ import com.model.relationship.client.model.ClientDTO;
 import com.model.relationship.client.repository.ClientRepository;
 import com.model.relationship.product.Repository.ProductRepository;
 import com.model.relationship.product.model.Product;
+import com.model.relationship.product.model.ProductRequestDTO;
+import com.model.relationship.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -24,6 +28,8 @@ public class ClientService {
 
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private ProductService productService;
 
     public void createClient(ClientDTO clientDTO) {
         Client client = new Client();
@@ -31,36 +37,60 @@ public class ClientService {
         client.setAddress(clientDTO.address());
         client.setEmail(clientDTO.email());
 
-        Client savedClient = clientRepository.save(client);
 
-        if (clientDTO.buyProducts() != null) {
+        Client savedClient = clientRepository.save(client);
+        if (clientDTO.products() != null) {
             buyProduct(clientDTO, savedClient);
         }
     }
 
+    public void updateClient(long id, ClientDTO clientDTO){
+        Client client = clientRepository.findById(id).get();
+        client.setName(clientDTO.name());
+        client.setEmail(clientDTO.email());
+        client.setAddress(clientDTO.address());
+        if (clientDTO.products() != null) {
+            buyProduct(clientDTO, client);
+        }
+    }
+
+    public void deleteClient(long id){
+        clientRepository.deleteById(id);
+    }
+
+    public ResponseEntity<List<Client>> getAllClients(){
+        return ResponseEntity.ok(clientRepository.findAll());
+    }
+
+    public ResponseEntity<Client> getClientById(long id){
+        return clientRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
     private void buyProduct(ClientDTO clientDTO, Client savedClient) {
-        for (BuyProductDTO buyProductDTO : clientDTO.buyProducts()) {
-            Optional<Product> productOpt = productRepository.findById(buyProductDTO.product().getId());
+        for (Product product : clientDTO.products()) {
+            Optional<BuyProduct> buyProduct = buyRepository.findById(product.getId());
 
-            if (productOpt.isPresent()) {
-                Product product = productOpt.get();
-
-                if (product.getProductQuantity() >= buyProductDTO.quantity()) {
-                    BuyProduct buyProduct = new BuyProduct();
-                    buyProduct.setClient(savedClient);
-                    buyProduct.setProduct(product);
-                    buyProduct.setQuantityPurchased(buyProductDTO.quantity());
-
-                    product.setProductQuantity(product.getProductQuantity() - buyProductDTO.quantity());
-                    productRepository.save(product);
-
-                    buyRepository.save(buyProduct);
-                } else {
-                    throw new IllegalArgumentException("Quantidade insuficiente para o produto: " + product.getId());
-                }
-            } else {
-                throw new IllegalArgumentException("Produto não encontrado: " + buyProductDTO.product().getId());
-            }
+//            if (productOpt.isPresent()) {
+//                Product productFound = productOpt.get();
+//
+//                if (product.getProductQuantity() >= productFound.getProductQuantity()) {
+//                    BuyProduct buyProduct = new BuyProduct();
+//                    buyProduct.setClient(savedClient);
+//                    buyProduct.setProduct(product);
+//                    buyProduct.setQuantityPurchased(product1.quantity());
+//
+//                    product.setProductQuantity(product.getProductQuantity() - product1.quantity());
+//                    productRepository.save(product);
+//
+//                    buyRepository.save(buyProduct);
+//                } else {
+//                    throw new IllegalArgumentException("Quantidade insuficiente para o produto: " + product.getId());
+//                }
+//            } else {
+//                throw new IllegalArgumentException("Produto não encontrado: " + product1.product().getId());
+//            }
         }
     }
 }
